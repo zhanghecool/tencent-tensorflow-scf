f9fcb2be7dd312236828beb18f58651131b04f27
He Zhang <zhanghecool@outlook.com>
2021-03-09 16:06:14 +0800

    feat(new): new scf.co

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
-use std::io::{self, Read};
-use ssvm_tensorflow_interface;
 use serde::Deserialize;
+use ssvm_tensorflow_interface;
+use std::io::{self, Read};
 
 fn main() {
     let model_data: &[u8] = include_bytes!("lite-model_aiy_vision_classifier_birds_V1_3.tflite");
     let labels = include_str!("aiy_birds_V1_labelmap.txt");
 
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).expect("Error reading from STDIN");
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("Error reading from STDIN");
     let obj: FaasInput = serde_json::from_str(&buffer).unwrap();
     // println!("{} {}", &(obj.body)[..5], obj.body.len());
     let img_buf = base64::decode_config(&(obj.body), base64::STANDARD).unwrap();
     // println!("Image buf size is {}", img_buf.len());
 
-    let flat_img = ssvm_tensorflow_interface::load_jpg_image_to_rgb8(&img_buf, 192, 192);
+    let flat_img = ssvm_tensorflow_interface::load_jpg_image_to_rgb8(&img_buf, 224, 224);
 
-    let mut session = ssvm_tensorflow_interface::Session::new(&model_data, ssvm_tensorflow_interface::ModelType::TensorFlowLite);
-    session.add_input("input", &flat_img, &[1, 192, 192, 3])
-           .run();
-    let res_vec: Vec<u8> = session.get_output("MobilenetV1/Predictions/Softmax");
+    let mut session = ssvm_tensorflow_interface::Session::new(
+        &model_data,
+        ssvm_tensorflow_interface::ModelType::TensorFlowLite,
+    );
+    session
+        .add_input(
+            "module/hub_input/images_uint8",
+            &flat_img,
+            &[1, 224, 224, 3],
+        )
+        .run();
+    let res_vec: Vec<u8> = session.get_output("module/prediction");
 
     let mut i = 0;
     let mut max_index: i32 = -1;
@@ -31,44 +41,37 @@ fn main() {
         }
         i += 1;
     }
-    // println!("{} : {}", max_index, max_value as f32 / 255.0);
 
-    /*
-    let mut confidence = "could be";
+    let mut confidence_zh = "可能有";
     if max_value > 200 {
-        confidence = "is very likely";
+        confidence_zh = "非常可能有";
     } else if max_value > 125 {
-        confidence = "is likely";
+        confidence_zh = "很可能有";
     } else if max_value > 50 {
-        confidence = "could be";
-    }
-    */
-    let mut confidence = "可能有";
-    if max_value > 200 {
-        confidence = "非常可能有";
-    } else if max_value > 125 {
-        confidence = "很可能有";
-    } else if max_value > 50 {
-        confidence = "可能有";
+        confidence_zh = "可能有";
     }
 
     let mut label_lines = labels.lines();
     for _i in 0..max_index {
-      label_lines.next();
+        label_lines.next();
     }
 
     let class_name = label_lines.next().unwrap().to_string();
-    if max_value > 50 && max_index != 0 {
-      // println!("It {} a <a href='https://cn.bing.com/search?q={}'>{}</a> in the picture", confidence.to_string(), class_name, class_name);
-      println!("上传的图片里面{} <a href='https://cn.bing.com/search?q={}'>{}</a>", confidence.to_string(), class_name, class_name);
+    if max_value > 50 {
+        // println!("It {} a <a href='https://cn.bing.com/search?q={}'>{}</a> in the picture", confidence.to_string(), class_name, class_name);
+        println!(
+            "上传的图片里面{} <a href='https://cn.bing.com/search?q={}'>{}</a>",
+            confidence_zh.to_string(),
+            class_name,
+            class_name
+        );
     } else {
-      // println!("It does not appears to be any birds item in the picture.");
-      println!("上传的图片里面没有检测到相关鸟类");
+        // println!("It does not appears to be any birds item in the picture.");
+        println!("上传的图片里面没有检测到相关鸟类");
     }
-    // println!("{} : {}", label_lines.next().unwrap().to_string(), confidence.to_string());
 }
 
 #[derive(Deserialize, Debug)]
 struct FaasInput {
-    body: String
+    body: String,
 }
